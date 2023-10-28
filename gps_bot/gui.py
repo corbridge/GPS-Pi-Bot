348fabc6e19e28e8867695346eaf71a2699a8284
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-10-28 13:39:14 -0600

    make gui a class

--- gps_bot/gui.py
+++ gps_bot/gui.py
@@ -1,42 +1,34 @@
 from tkinter import *
 import tkintermapview
 
-def add_marker_event(coords):
-    new_marker = map_widget.set_marker(coords[0], coords[1], text="position")
 
-root = Tk()
-root.title("GPS BOT GUI")
-# root.iconbitmap()
-root.geometry("700x550")
+class GPSGui:
+    def __init__(self):
+        root = Tk()
+        root.title("GPS BOT GUI")
+        # root.iconbitmap()
+        root.geometry("700x550")
 
-label = LabelFrame(root)
-label.pack(pady=20)
+        label = LabelFrame(root)
+        label.pack(pady=20)
 
-fime = [25.7252, -100.3135]
-fime_2 = [25.7258, -100.3138]
+        fime = [25.7252, -100.3135]
+        fime_2 = [25.7258, -100.3138]
 
-map_widget = tkintermapview.TkinterMapView(label, width=500, height=500, corner_radius=0)
-map_widget.set_position(fime[0], fime[1])
-map_widget.set_zoom(20)
+        self.map_widget = tkintermapview.TkinterMapView(label, width=500, height=500, corner_radius=0)
+        self.map_widget.set_position(fime[0], fime[1])
+        self.map_widget.set_zoom(20)
 
+        self.map_widget.add_right_click_menu_command(label="Add first position",
+                                                command=self.add_marker_event,
+                                                pass_coords=True)
 
-# first_position = map_widget.set_marker(fime[0], fime[1], text="First position")
-# second_position = map_widget.set_marker(fime_2[0], fime_2[1], text="Second position")
+        self.map_widget.add_right_click_menu_command(label="Add second position",
+                                                command=self.add_marker_event,
+                                                pass_coords=True)
 
-map_widget.add_right_click_menu_command(label="Add first position",
-                                        command=add_marker_event,
-                                        pass_coords=True)
-
-map_widget.add_right_click_menu_command(label="Add second position",
-                                        command=add_marker_event,
-                                        pass_coords=True)
-
-# polygon_1 = map_widget.set_polygon([(fime[0], fime[1]),
-#                                     (fime_2[0], fime_2[1])],
-#                                    # fill_color=None,
-#                                    # outline_color="red",
-#                                    # border_width=12,
-#                                    name="path")
-
-map_widget.pack()
-root.mainloop()
+        self.map_widget.pack()
+        root.mainloop()
+    
+    def add_marker_event(self,coords):
+        new_marker = self.map_widget.set_marker(coords[0], coords[1], text="position")
