8c94f33aa28efd8297f1d248010176dbc616f6cc
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-10-29 23:33:47 -0600

    make gps class

--- gps_bot/gps.py
+++ gps_bot/gps.py
@@ -2,42 +2,40 @@ import serial
 from time import sleep
 
 
-def GPS_Info():
+class InterfaceGPS:
+    def __init__(self):
+        gpgga_info = "$GPGGA,"
+        ser = serial.Serial ("/dev/ttyAMA0")              #Open port with baud rate
+        GPGGA_buffer = 0
+        NMEA_buff = 0
+        received_data = (str)(ser.readline())
+        GPGGA_data_available = received_data.find(gpgga_info)                
+        if (GPGGA_data_available>0):
+            GPGGA_buffer = received_data.split("$GPGGA,",1)[1]
+            NMEA_buff = (GPGGA_buffer.split(','))
+            self.latitude, self.longitude = self.GPS_Info(NMEA_buff)
 
-    nmea_latitude = []
-    nmea_longitude = []
-    nmea_latitude = NMEA_buff[1]
-    nmea_longitude = NMEA_buff[3]
-    
-    lat = float(nmea_latitude)
-    longi = float(nmea_longitude)
-    
-    lat_in_degrees = convert_to_degrees(lat)
-    long_in_degrees = convert_to_degrees(longi)
-    return lat_in_degrees, long_in_degrees
-    
-#convert raw NMEA string into degree decimal format   
-def convert_to_degrees(raw_value):
-    decimal_value = raw_value/100.00
-    degrees = int(decimal_value)
-    mm_mmmm = (decimal_value - int(decimal_value))/0.6
-    position = degrees + mm_mmmm
-    position = "%.4f" %(position)
-    return position
+    def GPS_Info(self, NMEA_buff):
 
-
-gpgga_info = "$GPGGA,"
-ser = serial.Serial ("/dev/ttyAMA0")              #Open port with baud rate
-GPGGA_buffer = 0
-NMEA_buff = 0
+        nmea_latitude = []
+        nmea_longitude = []
+        nmea_latitude = NMEA_buff[1]
+        nmea_longitude = NMEA_buff[3]
+        
+        lat = float(nmea_latitude)
+        longi = float(nmea_longitude)
+        
+        lat_in_degrees = self.convert_to_degrees(lat)
+        long_in_degrees = self.convert_to_degrees(longi)
+        return lat_in_degrees, long_in_degrees
+    
+    def convert_to_degrees(self, raw_value):
+        decimal_value = raw_value/100.00
+        degrees = int(decimal_value)
+        mm_mmmm = (decimal_value - int(decimal_value))/0.6
+        position = degrees + mm_mmmm
+        position = "%.4f" % (position)
+        return position
 
 while True:
-    received_data = (str)(ser.readline())                   #read NMEA string received
-    GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string                 
-    if (GPGGA_data_available>0):
-        GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after "$GPGGA," string 
-        NMEA_buff = (GPGGA_buffer.split(','))               #store comma separated data in buffer
-        lattitude, longuitud = GPS_Info()                                          #get time, latitude, longitude
-
-        print("lat in degrees:", lattitude," long in degree: ", longuitud, '\n')
-
+    InterfaceGPS()
--- gps_bot/gui.py
+++ gps_bot/gui.py
@@ -3,7 +3,10 @@ import tkintermapview
 
 
 class GPSGui:
-    def __init__(self):
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
         root = Tk()
         root.title("GPS BOT GUI")
         # root.iconbitmap()
@@ -21,11 +24,9 @@ class GPSGui:
         start_button = Button(root, text="Camera", command=self.click_camera)
         start_button.place(x=20, y=105)
 
-        fime = [25.7252, -100.3135]
-        fime_2 = [25.7258, -100.3138]
 
         self.map_widget = tkintermapview.TkinterMapView(label, width=500, height=500, corner_radius=0)
-        self.map_widget.set_position(fime[0], fime[1])
+        self.map_widget.set_position(self.latitude, self.longitude)
         self.map_widget.set_zoom(20)
 
         self.map_widget.add_right_click_menu_command(label="Add first position",
