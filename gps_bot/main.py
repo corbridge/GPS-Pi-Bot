1f8de20422fd6a4cfe30b76056133d152557b091
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-11-12 17:06:32 -0600

    getg rid of firebase stuff

--- gps_bot/main.py
+++ gps_bot/main.py
@@ -1,9 +1,5 @@
 from gps import InterfaceGPS
-from firebase_config import firebaseInit
-
-db = firebaseInit()
 
 gps = InterfaceGPS()
 while True:
     cordenates = gps.get_position()
-    db.update(cordenates)
