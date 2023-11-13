c8008a5f726f7220e1f26daf685e18dee32d886f
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-11-12 22:46:39 -0600

    make gps tracker work better (hope)

--- gps_bot_app/index.js
+++ gps_bot_app/index.js
@@ -7,25 +7,17 @@ var mark;
 var lineCoords = [];
 
 var initialize = function() {
- map  = new google.maps.Map(document.getElementById('map'), {center:{lat:lat,lng:lng},zoom:20});
- mark = new google.maps.Marker({position:{lat:lat, lng:lng}, map:map});
+ map  = new google.maps.Map(document.getElementById('map'), {center:{lat:latitude,lng:longitude},zoom:20});
+ mark = new google.maps.Marker({position:{lat:latitude, lng:longitude}, map:map});
 };
 
 window.initialize = initialize;
-const spawner = require('child_process').spawn;
 
-const data_pass = ['Send it'];
+console.log(latitude);
+console.log(longitude);
 
-console.log('Data sent: ', data_pass);
-
-const python_process = spawner('python', ['/home/picornelio/projects/GPS-Pi-Bot/gps_bot/main.py', JSON.stringify(data_pass)]);
-
-python_process.stdout.on('data', (data) => {
-  console.log('data received: ', JSON.parse(data)[1]);
-});
-
-map.setCenter({lat:JSON.parse(data)[1].latitude, lng:JSON.parse(data)[1].longitude, alt:0});
-mark.setPosition({lat:JSON.parse(data)[1].latitude, lng:JSON.parse(data)[1].longitude, alt:0});
+map.setCenter({lat:latitude, lng:longitude, alt:0});
+mark.setPosition({lat:latitude, lng:longitude, alt:0});
 
 var lineCoordinatesPath = new google.maps.Polyline({
    path: lineCoords,
@@ -34,3 +26,4 @@ var lineCoordinatesPath = new google.maps.Polyline({
  });
 
 lineCoordinatesPath.setMap(map);
+
