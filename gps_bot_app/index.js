6c59a7552d9ae7fccb0793ad94675990da6964c4
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-11-12 20:20:22 -0600

    add good js code

--- gps_bot_app/index.js
+++ gps_bot_app/index.js
@@ -1,50 +1,36 @@
+
 window.lat = 25.7;
 window.lng = -100.3;
 
 var map;
 var mark;
 var lineCoords = [];
-  
+
 var initialize = function() {
-  map  = new google.maps.Map(document.getElementById('map'), {center:{lat:lat,lng:lng},zoom:20});
-  mark = new google.maps.Marker({position:{lat:lat, lng:lng}, map:map});
+ map  = new google.maps.Map(document.getElementById('map'), {center:{lat:lat,lng:lng},zoom:20});
+ mark = new google.maps.Marker({position:{lat:lat, lng:lng}, map:map});
 };
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCndLrX1QvxSug-lwX5-LfV1pTs_IegH3s",
-    authDomain: "gps-pi-bot.firebaseapp.com",
-    databaseURL: "https://gps-pi-bot-default-rtdb.firebaseio.com",
-    projectId: "gps-pi-bot",
-    storageBucket: "gps-pi-bot.appspot.com",
-    messagingSenderId: "237945598717",
-    appId: "1:237945598717:web:3143f5d47f0f8a8fb84494"
-  };
-
 window.initialize = initialize;
+const spawner = require('child_process').spawn;
 
-firebase.initializeApp(firebaseConfig );
+const data_pass = ['Send it'];
 
-var ref = firebase.database().ref();
+console.log('Data sent: ', data_pass);
 
-ref.on('value', function(snapshot) {
-  var gps = snapshot.val();
-  console.log(gps.latitude);
-  console.log(gps.longitude);
-  lat = gps.latitude;
-  lng = gps.longitude;
+const python_process = spawner('python', ['/home/picornelio/projects/GPS-Pi-Bot/gps_bot/main.py', JSON.stringify(data_pass)]);
 
-  map.setCenter({lat:lat, lng:lng, alt:0});
-  mark.setPosition({lat:lat, lng:lng, alt:0});
-  
-  lineCoords.push(new google.maps.LatLng(lat, lng));
-
-  var lineCoordinatesPath = new google.maps.Polyline({
-    path: lineCoords,
-    geodesic: true,
-    strokeColor: '#2E10FF'
-  });
-  
-  lineCoordinatesPath.setMap(map);
-}, function (error) {
-  console.log("Error: " + error.code);
+python_process.stdout.on('data', (data) => {
+  console.log('data received: ', JSON.parse(data)[1]);
 });
+
+map.setCenter({lat:JSON.parse(data)[1].latitude, lng:JSON.parse(data)[1].longitude, alt:0});
+mark.setPosition({lat:JSON.parse(data)[1].latitude, lng:JSON.parse(data)[1].longitude, alt:0});
+
+var lineCoordinatesPath = new google.maps.Polyline({
+   path: lineCoords,
+   geodesic: true,
+   strokeColor: '#2E10FF'
+ });
+
+lineCoordinatesPath.setMap(map);
