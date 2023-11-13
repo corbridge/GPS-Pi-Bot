31730d37612c659e36a9f8c0297d3e0675dfe467
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-11-13 00:08:43 -0600

    trying to check variables every second

--- gps_bot_app/index.js
+++ gps_bot_app/index.js
@@ -1,4 +1,5 @@
 
+
 window.lat = 25.7;
 window.lng = -100.3;
 
@@ -6,18 +7,28 @@ var map;
 var mark;
 var lineCoords = [];
 
+function updateVariables(){
+
+var lat = latitude;
+var long = longitude;
+console.log(lat);
+console.log(long);
+
+}
+
 var initialize = function() {
- map  = new google.maps.Map(document.getElementById('map'), {center:{lat:latitude,lng:longitude},zoom:20});
- mark = new google.maps.Marker({position:{lat:latitude, lng:longitude}, map:map});
+ var lat = latitude;
+ var long = longitude;
+
+ map  = new google.maps.Map(document.getElementById('map'), {center:{lat:lat,lng:long},zoom:20});
+ mark = new google.maps.Marker({position:{lat:lat, lng:long}, map:map});
+
 };
 
 window.initialize = initialize;
 
-console.log(latitude);
-console.log(longitude);
-
-map.setCenter({lat:latitude, lng:longitude, alt:0});
-mark.setPosition({lat:latitude, lng:longitude, alt:0});
+map.setCenter({lat:lat, lng:long, alt:0});
+mark.setPosition({lat:lat, lng:long, alt:0});
 
 var lineCoordinatesPath = new google.maps.Polyline({
    path: lineCoords,
