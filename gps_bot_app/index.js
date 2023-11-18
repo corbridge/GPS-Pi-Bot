b4674535e3b4ddc5de793393d6d2c37efb51ea8e
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-11-17 23:51:37 -0600

    first method comes back

--- gps_bot_app/index.js
+++ gps_bot_app/index.js
@@ -1,40 +1,16 @@
+function initialize(){
 
-//hi
-window.lat = 25.7;
-window.lng = -100.3;
+  var cord = {lat:latitude, lng:longitude};
+  var map = new google.maps.Map(
+    document.getElementById('map'),
+    {zoom:20,
+    center:cord
+    }
+  );
 
-var map;
-var mark;
-var lineCoords = [];
-
-function updateVariables(){
-
-var lat = latitude;
-var long = longitude;
-console.log(lat);
-console.log(long);
+  var marker = new google.maps.Marker({
+    position:cord,
+    map: map
+  })
 
 }
-
-var initialize = function() {
- var lat = latitude;
- var long = longitude;
-
- map  = new google.maps.Map(document.getElementById('map'), {center:{lat:lat,lng:long},zoom:20});
- mark = new google.maps.Marker({position:{lat:lat, lng:long}, map:map});
-
-};
-
-window.initialize = initialize;
-
-map.setCenter({lat:lat, lng:long, alt:0});
-mark.setPosition({lat:lat, lng:long, alt:0});
-
-var lineCoordinatesPath = new google.maps.Polyline({
-   path: lineCoords,
-   geodesic: true,
-   strokeColor: '#2E10FF'
- });
-
-lineCoordinatesPath.setMap(map);
-
