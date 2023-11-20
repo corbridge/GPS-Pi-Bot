450023b393a2971bb43316b6e5f96689b12cded2
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-11-20 16:42:51 -0600

    check smth again

--- gps_bot/main.py
+++ gps_bot/main.py
@@ -9,18 +9,21 @@ lat2, lon2 = 25.7422753, -100.2794433  # san Nico
 
 while True:
     cordenates = gps.get_position()
-    #gps.sending_data_js(cordenates)
-
-    distance = gps.distanceBetween(cordenates['latitude'], cordenates['longitude'],lat2, lon2)
-    angle = gps.course_to(cordenates['latitude'], cordenates['longitude'],lat2, lon2)
-
-    # if angle == 0:
-    if distance <= 0:
-        #direction.neutral()
-        print(f'La distancia es: {distance}')
+    if cordenates is None:
+        pass
     else:
-        #direction.foward()
-        print(f'La distancia es: {distance}')
-    # else:
-#        direction.neutral()
-        # print(f'Ajuste el vehiculo {angle} grados')
+        #gps.sending_data_js(cordenates)
+
+        distance = gps.distanceBetween(cordenates['latitude'], cordenates['longitude'],lat2, lon2)
+        angle = gps.course_to(cordenates['latitude'], cordenates['longitude'],lat2, lon2)
+
+        # if angle == 0:
+        if distance <= 0:
+            #direction.neutral()
+            print(f'La distancia es: {distance}')
+        else:
+            #direction.foward()
+            print(f'La distancia es: {distance}')
+        # else:
+    #        direction.neutral()
+            # print(f'Ajuste el vehiculo {angle} grados')
