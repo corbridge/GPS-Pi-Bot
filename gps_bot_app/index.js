63372c22c373ef1e3176681d3343c4ff1058dda5
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-11-19 22:49:11 -0600

    add another marker

--- gps_bot_app/index.js
+++ gps_bot_app/index.js
@@ -1,6 +1,6 @@
 function initialize(){
 
-  var cord = {lat:latitude, lng:longitude};
+  var cord = {lat:25.73404, lng:-100.2996872};
   var map = new google.maps.Map(
     document.getElementById('map'),
     {zoom:20,
@@ -9,8 +9,34 @@ function initialize(){
   );
 
   var marker = new google.maps.Marker({
+    icon:'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
     position:cord,
     map: map
   })
 
+
+  google.maps.event.addListener(map, "click", (event) => {
+    addListing(event.latLng, map);
+  });
+}
+
+function addMarker(location, map) {
+  new google.maps.Marker({
+    position: location,
+    label: 'D',
+    map: map,
+    draggable: true
+  });
+}
+
+
+var i=0
+function addListing(location, map) {
+  var iMax=1;
+
+  if(i<iMax) {
+    addMarker(location, map);
+    i++;
+    console.log('count: ',i);
+  }
 }
